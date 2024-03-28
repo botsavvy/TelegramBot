2fe48376a361494c1025962e65d665381114c3c0
zainbalouch <zainuleman786@gmail.com>
2024-03-28 21:27:30 +0500

    worldClock bot done

--- zs_test_3_bot.py
+++ zs_test_3_bot.py
@@ -7,6 +7,9 @@ from timezonefinder import TimezoneFinder
 # Set up the bot with your API token
 bot = telebot.TeleBot("6997327762:AAG0TTlHe0Rx2A5w9OsKJRYYvIGqtBXDLqA")
 
+# Dictionary to store the world time mode status for each user
+user_world_time_mode = {}
+
 # Function to get the current time in a given city
 def get_world_time(city_name):
     fmt = '%A %Y-%m-%d %H:%M'
@@ -35,27 +38,28 @@ def get_world_time(city_name):
 # Command handler for /tell_world_time
 @bot.message_handler(commands=['tell_world_time'])
 def tell_world_time(message):
-    bot.reply_to(message, "Please enter the city name:")
-    bot.register_next_step_handler(message, process_city_name_step)
-
-# Handler for city name input
-def process_city_name_step(message):
-    try:
-        city_name = message.text.strip()
-        time_info = get_world_time(city_name)
-        bot.reply_to(message, time_info)
-    except Exception as e:
-        bot.reply_to(message, f"An error occurred: {str(e)}")
+    user_world_time_mode[message.chat.id] = True
+    bot.reply_to(message, "You have turned on World Time mode.\nPlease enter the city name:")
 
 # Command handler for /close_world_time
 @bot.message_handler(commands=['close_world_time'])
 def close_world_time(message):
+    if message.chat.id in user_world_time_mode:
+        del user_world_time_mode[message.chat.id]
     bot.reply_to(message, "World time feature disabled. Bot will now echo your messages.")
 
 # Echo handler for normal messages
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    if message.chat.id in user_world_time_mode:
+        try:
+            city_name = message.text.strip()
+            time_info = get_world_time(city_name)
+            bot.reply_to(message, f'{time_info}\nTo turn Off World time mode type command ("/close_world_time") and bot will echo your messages')
+        except Exception as e:
+            bot.reply_to(message, f"An error occurred: {str(e)}\n To turn Off World time mode type command ('/close_world_time') and bot will echo your messages")
+    else:
+        bot.reply_to(message, f'{message.text}\nTo turn on world time mode please enter command ("/tell_world_time")')
 
 # Start the bot
 bot.polling()
